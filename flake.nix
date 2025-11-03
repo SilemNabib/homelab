7ded0bdf0a1d3c0cc2e89cdd14f6ecb2dcc18fe2
Silem Nabib <silemnabib@gmail.com>
2025-11-03 16:10:09 -0500

    feat: enhance NixOS configuration focusing modules

--- flake.nix
+++ flake.nix
@@ -1,33 +1,67 @@
 {
-  description = "Homelab NixOS Flake";
+  description = "Homelab by Silem - 1.0v";
+
+  nixConfig = {
+    experimental-features = [ "nix-command" "flakes" ];
+    extra-substituters = [ "https://cache.nixos.org/" ];
+    extra-trusted-public-keys = [];
+  };
 
   inputs = {
+    # Oficial NixOS Repository
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    # Disko
-    disko.url = "github:nix-community/disko";
-    disko.inputs.nixpkgs.follows = "nixpkgs";
+
+    # Disko - NixOS module for managing disks and filesystems
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # Flake Parts - Helper library for sort Nix flakes
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = { self, nixpkgs, disko, ... }@inputs: let
-    nodes = [
-      "beacon-0"
-    ];
-  in {
-    nixosConfigurations = builtins.listToAttrs (map (name: {
-	    name = name;
-	    value = nixpkgs.lib.nixosSystem {
-     	    specialArgs = {
-            meta = { hostname = name; };
-          };
-          system = "x86_64-linux";
-          modules = [
-              # Modules
-	            disko.nixosModules.disko
-	            ./hardware-configuration.nix
-	            ./disko-config.nix
-	            ./configuration.nix
-	          ];
+  outputs = inputs@{ self, nixpkgs, disko, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+
+      # Define supported systems
+      systems = [ "x86_64-linux" ];
+
+      perSystem = { system, pkgs, ... }: {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [ git nixfmt ];
         };
-    }) nodes);
-  };
+      };
+
+      flake = {
+        nixosConfigurations = let
+          nodes = [ "beacon-0" ];
+
+          mkHost = name: nixpkgs.lib.nixosSystem {
+            inherit system;
+
+            specialArgs = {
+              meta = { hostname = name; };
+            };
+            
+            modules = [
+              disko.nixosModules.disko
+              ./nixos/hosts/${name}/hardware-configuration.nix
+              ./nixos/hosts/${name}/disko.nix
+              ./nixos/hosts/${name}/configuration.nix
+              ./nixos/modules/common.nix
+            ];
+          };
+          in
+          # For each node, create a NixOS configuration
+          builtins.listToAttrs (map (name: {
+            name = name;
+            value = mkHost name;
+          }) nodes);
+        
+        # Set default package to the beacon-0 NixOS configuration if the user runs `nix build` without any arguments
+        defaultPackage.x86_64-linux =
+          self.nixosConfigurations.beacon-0.config.system.build.toplevel;
+      };
+    };
 }
