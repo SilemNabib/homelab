d6c12f3caf696897622186affd3fc00d8c43b116
Silem Nabib <silemnabib@gmail.com>
2026-04-29 20:59:09 -0500

    chore: wipeout

--- nixos/users/default.nix
+++
@@ -1,7 +0,0 @@
-{ config, pkgs, lib, meta, ... }:
-
-{
-  # Default user configuration
-  # This is used when no specific user configuration exists for the host type
-  # You can leave this empty or add default users here
-}
